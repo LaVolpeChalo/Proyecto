ced14fc90f597693b207068272209bb71bd123f7
Esteban-688 <esteban.rodriguez.t@mail.pucv.cl>
2021-06-23 13:10:18 -0400

    Estetica Main
    Se mejor la estetica del main.c

--- main.c
+++ main.c
@@ -21,28 +21,48 @@ int main(){
         
         switch(opcion){
             case 1: importarArchivo(mapa);
+            Printf(green"\n Importacion de archivo \n"reset);
                 break;
             case 2:
+            Printf(green"\n Buscar Vehiculo \n"reset);
+            //Buscar Vehiculos
                 break;
             case 3:
+            Printf(green"\n Buscar Vehiculo por Nombre \n"reset);
+            //Buscar Vehiculos por nombre
                 break;
             case 4:
+            Printf(green"\n Agregar Vehiculo\n"reset);
+            //Agregar Vehiculo
                 break;
             case 5:
+            Printf(green"\n Ingresar Auto a Lista de Autos Deseados\n"reset);
+            //Ingresar Auto a lista de Autos Deseados
                 break;
             case 6:
+            Printf(green"\n Lista de Autos Deseados \n"reset);
+            //Desplegar lista de Autos Deseados
                 break; 
             case 7: 
+            Printf(green"\n Todos los autos \n"reset);
                 if(mapsize(mapa) != 0) desplegarautos(mapa);
                 else printf(red"\nLa base de datos se encuentra vacia\n"reset);
                 break;
             case 8:
+            Printf(green"\n Compra \n"reset);
+            //Concretar Compra
                 break;
             case 9:
+            Printf(green"\n Elimininación de Autos Deseados\n"reset);
+            //Eliminar Auto de la lista de Autos Deseados
                 break;
             case 10:
+            Printf(green"\n Eliminacion de Autos Base de Datos \n"reset);
+            // Eliminar Auto de la base de Datos
                 break;
             case 11:
+            Printf(green"\n Exportar CSV \n"reset);
+            //Exportar csv
                 break;                                    
         }
 
